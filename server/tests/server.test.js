9be6d5448fe5ed6dc21c76f448205cbfb3f0ec4e
Brian Gonzalez <brianfgonzalez@gmail.com>
2017-12-18 17:59:01 -0500

    GET /todos test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest')
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
 
+const todos = [{
+  text:  'First test todo'
+}, {
+  text: 'Second test todo'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -23,8 +31,8 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) return done(err)
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1)
-          expect(todos[0].text).toBe(text)
+          expect(todos.length).toBe(3)
+          expect(todos[2].text).toBe(text)
           done()
         }).catch((err) => done(err))
       })
@@ -38,9 +46,23 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) return done(err)
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos).toHaveLength(2)
           done()
         }).catch((err) => done(err))
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should pull two seeded todos from local Todo mongoDB', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos).toHaveLength(2)
+        expect(res.body.todos[0].text).toEqual(todos[0].text)
+        expect(res.body.todos[1].text).toEqual(todos[1].text)
+      })
+      .end(done)
   })
 })
