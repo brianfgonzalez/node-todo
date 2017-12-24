3b9b1f7e9e941ed41aabfadd1fe4d5dc1bac11b9
Brian Gonzalez <brianfgonzalez@gmail.com>
2017-12-23 21:24:40 -1000

    Added MongoClient options

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,17 @@ var mongoose = require('mongoose')
 // http://mongoosejs.com/docs/guide.html
 
 // Use bluebird
-mongoose.Promise = require('bluebird')
+// mongoose.Promise = require('bluebird')
 //var options = { promiseLibrary: require('bluebird') }
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
+var options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', options)
 
 module.exports = {mongoose}
