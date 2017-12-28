3764a9d040f4636deafdf3f30fc38119cd336659
Brian Gonzalez <brianfgonzalez@gmail.com>
2017-12-28 17:18:02 -0500

    Added catch to todo promises

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -14,6 +14,7 @@ var options = {
   bufferMaxEntries: 0,
   promiseLibrary: require('mpromise')
 };
+mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', options)
 
 module.exports = {mongoose}
--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.post('/todos', (req, res) => {
 })
 
 app.post('/users', (req, res) => {
-    var todo = new User({
+    var user = new User({
       email: req.body.email
     })
-    todo.save().then((doc) => {
+    user.save().then((doc) => {
       res.send(doc)
     }, (e) => {
       res.status(400).send(e)
@@ -36,9 +36,7 @@ app.post('/users', (req, res) => {
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})
-  }, (e) => {
-    res.status(400).send(e)
-  })
+  }.catch((e) => res.status(400).send(e))
 })
 
 app.get('/todos/:todoid', (req, res) => {
@@ -48,9 +46,7 @@ app.get('/todos/:todoid', (req, res) => {
   Todo.findById(id).then((todo) => {
     if (!todo) return res.status(400).send('Todo is not found in mangoDB')
     res.send({todo})
-  }, (e) => {
-    res.status(400).send(e)
-  })
+  }.catch((e) => res.status(400).send(e))
 })
 
 app.get('/users/:userid', (req, res) => {
@@ -68,15 +64,11 @@ app.get('/users/:userid', (req, res) => {
 app.delete('/todos/:todoid', (req, res) => {
   var id = req.params.todoid
 
-  if (!ObjectID.isValid(id)) res.status(400).send(`Passed todo id(${id}) is not valid`)
+  if (!ObjectID.isValid(id)) res.status(400).send(`Passed todo id "${id}" is not valid`)
   Todo.findByIdAndRemove(id).then((todo) => {
     if (!todo) return res.status(404).send('Todo is not found in mongoDB')
     res.send({todo})
-  }, (e) => res.status(400).send(e))
-  // validate id send 404
-  // remove todo by id
-  // success(if no doc send 404, id not found) or error(400, empty body)
-
+  }).catch((e) => res.status(400).send(e))
 })
 
 app.listen(port, () => {
