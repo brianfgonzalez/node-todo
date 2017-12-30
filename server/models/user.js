fee6962ec682774e1ef3eabfe69422f2ac8840a8
Brian Gonzalez <brianfgonzalez@gmail.com>
2017-12-30 06:28:34 -0500

    LOGIN POST route added

--- server/models/user.js
+++ server/models/user.js
@@ -75,6 +75,19 @@ UserSchema.statics.findByToken = function (token) {
   })
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  return User.findOne({email: email}).then((user) => {
+    if (!user) return Promise.reject('User is not found')
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (!res) reject('Password is incorrect')
+        resolve(user)
+      })
+    })
+
+  })
+}
+
 UserSchema.pre('save', function (next) {
   var user = this
 
