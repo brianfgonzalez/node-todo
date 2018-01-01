4c0fe6a9aa3793ccb6ce04505fa34dd5f2ae121f
Brian Gonzalez <brianfgonzalez@gmail.com>
2018-01-01 03:34:19 -0500

    TEST case for delete token route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -286,6 +286,23 @@ describe('USERS', () => {
     })
 
   })
+
+  describe('DELETE /users/me/token', () => {
+    it('should remove auth token', (done) => {
+      request(app)
+        .delete('/users/me/token')
+        .set('x-auth', users[0].tokens[0].token)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err)
+
+          User.findById(users[0]._id).then((user) => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          }).catch((e) => done(e))
+        })
+    })
+  })
 })
 //  case sensitive: variableABC = variableABC.replace(/B/g, "D");
 //  case insensitive: variableABC = variableABC.replace(/B/gi, "D");
